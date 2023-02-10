aace563e0bb9c5678acdce65e81cb6df4aacabbc
Shriram <shriramathreya786@gmail.com>
2023-02-10 14:04:55 -0600

    flake8 fix

--- servicex_codegen/post_operation.py
+++ servicex_codegen/post_operation.py
@@ -36,7 +36,7 @@ import os
 import zipfile
 from tempfile import TemporaryDirectory
 
-from flask import Response, current_app, request
+from flask import Response, request
 from flask_restful import Resource
 from requests_toolbelt import MultipartEncoder
 
